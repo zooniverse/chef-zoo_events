a0576ca3adc28a5b70962f304814fbba7201a80f
Edward Paget <ed.paget@gmail.com>
2014-03-19 15:19:21 -0500

    point to github zoo-base update zoo-live version

--- Berksfile
+++ Berksfile
@@ -2,7 +2,7 @@ site :opscode
 
 metadata
 
-cookbook 'zoo-base', path: '../zoo-base'
+cookbook 'zoo-base', git: 'git@github.com:zooniverse/zoo-base.git'
 cookbook 'monit', github: 'phlipper/chef-monit', tag: '1.5.2'
 cookbook 'hostname', github: 'edpaget/chef-cookbook-hostname', branch: "dev"
 cookbook 'clojure_web', path: '../chef-clojure_web'
--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 override['set_fqdn'] = '*.zooniverse.org'
 override['clojure_web']['app_name'] = 'zoo-live'
-override['clojure_web']['app_version'] = '0.2.0-SNAPSHOT'
+override['clojure_web']['app_version'] = '0.3.0-SNAPSHOT'
 override['clojure_web']['artefact'] = "http://ubret.s3.amazonaws.com/#{node['clojure_web']['app_name']}-#{node['clojure_web']['app_version']}.jar"
 override['clojure_web']['conf'] = "http://ubret.s3.amazonaws.com/#{node['clojure_web']['app_name']}-#{node['clojure_web']['app_version']}.edn"
